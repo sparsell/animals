6adb6f8a050e0deb4f7d7eea60ca6ae9ed90ce9f
sparsell <hello@sparsell.com>
2023-05-09 05:54:43 -0400

    adds button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-    return <div>Show animal detail here</div>
+
+    const handleClick = () => {
+        console.log("button was clicked")
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add animal</button>
+        </div>
+    )
 }
 
 export default App; 
